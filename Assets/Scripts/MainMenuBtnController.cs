35581a9bdc4fe76ce34acc37e25a8a9eb86233bc
Brian <46753834+Shankk@users.noreply.github.com>
2021-02-26 18:40:35 -0500

    Update #1
    Added Main Menu, Player HUD, Pause Menu
    Added Main Menu Scene & LevelOne Scene
    Added Player Prefab

---
+++ Assets/Scripts/MainMenuBtnController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBtnController : MonoBehaviour
+{
+    [SerializeField]
+    Canvas MainMenu;
+    [SerializeField]
+    Canvas Options;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void OpenOptions()
+    {
+        MainMenu.GetComponent<Canvas>().enabled = false;
+        Options.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void CloseOptions()
+    {
+        Options.GetComponent<Canvas>().enabled = false;
+        MainMenu.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void ExitGameApp()
+    {
+        Debug.Log("Quiting Game Application");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseBtnController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBtnController : MonoBehaviour
+{
+    [SerializeField]
+    Canvas PauseUi;
+    [SerializeField]
+    Canvas PlayerUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResumeGame() 
+    {
+        Debug.Log("Resuming Game...");
+        Time.timeScale = 1;
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
